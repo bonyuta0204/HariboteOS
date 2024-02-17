acb4302e5532e92a33b498fbbb5703763c577218
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-02-18 01:17:28 +0900

    refactor logic regarding mouse

--- bootpack.c
+++ bootpack.c
@@ -3,16 +3,21 @@
 
 #include "bootpack.h"
 
+struct MOUSE_DEC {
+  unsigned char buf[3], phase;
+};
 extern struct FIFO8 keyfifo;
 extern struct FIFO8 mousefifo;
-void enable_mouse(void);
+void enable_mouse(struct MOUSE_DEC *mdec);
 void init_keyboard(void);
+
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char data);
 
 void HariMain(void) {
   struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
   char s[40], mcursor[256], keybuf[32], mousebuf[128];
   int mx, my, i;
-  unsigned char mouse_dbuf[3], mouse_phase;
+  struct MOUSE_DEC mdec;
 
   init_gdtidt();
   init_pic();
@@ -35,8 +40,7 @@ void HariMain(void) {
   sprintf(s, "(%d, %d)", mx, my);
   putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
-  enable_mouse();
-  mouse_phase = 0; /* マウスの0xfaを待っている段階へ */
+  enable_mouse(&mdec);
 
   for (;;) {
     io_cli();
@@ -55,28 +59,11 @@ void HariMain(void) {
         i = fifo8_get(&mousefifo);
         io_sti();
 
-        switch (mouse_phase) {
-        case 0:
-          if (i == 0xfa) {
-            mouse_phase = 1;
-          };
-          break;
-        case 1:
-          mouse_dbuf[0] = i;
-          mouse_phase = 2;
-          break;
-        case 2:
-          mouse_dbuf[1] = i;
-          mouse_phase = 3;
-          break;
-        case 3:
-          mouse_dbuf[2] = i;
-          mouse_phase = 1;
-          sprintf(s, "%d %d %d", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
+        if (mouse_decode(&mdec, i) != 0) {
+          sprintf(s, "%d %d %d", mdec.buf[0], mdec.buf[1], mdec.buf[2]);
           boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16,
                    32 + 8 * 8 - 1, 31);
           putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
-          break;
         }
       }
     }
@@ -112,11 +99,36 @@ void init_keyboard(void) {
 #define KEYCMD_SENDTO_MOUSE 0xd4
 #define MOUSECMD_ENABLE 0xf4
 
-void enable_mouse(void) {
+void enable_mouse(struct MOUSE_DEC *mdec) {
   /* マウス有効 */
   wait_KBC_sendready();
   io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
   wait_KBC_sendready();
   io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
-  return; /* うまくいくとACK(0xfa)が送信されてくる */
+  /* うまくいくとACK(0xfa)が送信されてくる */
+  mdec->phase = 1;
+
+  return;
+}
+
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char data) {
+  switch (mdec->phase) {
+  case 0:
+    if (data == 0xfa) {
+      mdec->phase = 1;
+    };
+    return 0;
+  case 1:
+    mdec->buf[0] = data;
+    mdec->phase = 2;
+    return 0;
+  case 2:
+    mdec->buf[1] = data;
+    mdec->phase = 3;
+    return 0;
+  case 3:
+    mdec->buf[2] = data;
+    mdec->phase = 1;
+    return 1;
+  }
 }
