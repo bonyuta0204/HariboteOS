35f64e03f15751eef93853fe43e18c2de9ade5b2
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-02-17 15:20:01 +0900

    Add print string function

--- bootpack.c
+++ bootpack.c
@@ -14,6 +14,8 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0,
 
 void init_screen(char *vram, int xsize, int ysize);
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c,
+                   unsigned char *s);
 
 #define COL8_000000 0
 #define COL8_FF0000 1
@@ -43,18 +45,14 @@ void HariMain(void) {
 
   char *vram;
   int xsize, ysize;
-  extern char hankaku[4096];
 
   binfo = (struct BOOTINFO *)0x0ff0;
 
   init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
 
-  putfont8(binfo->vram, binfo->scrnx, 8, 8, COL8_FFFFFF, hankaku + 'A' * 16);
-  putfont8(binfo->vram, binfo->scrnx, 16, 8, COL8_FFFFFF, hankaku + 'B' * 16);
-  putfont8(binfo->vram, binfo->scrnx, 24, 8, COL8_FFFFFF, hankaku + 'C' * 16);
-  putfont8(binfo->vram, binfo->scrnx, 40, 8, COL8_FFFFFF, hankaku + '1' * 16);
-  putfont8(binfo->vram, binfo->scrnx, 48, 8, COL8_FFFFFF, hankaku + '2' * 16);
-  putfont8(binfo->vram, binfo->scrnx, 56, 8, COL8_FFFFFF, hankaku + '3' * 16);
+  putfonts8_asc(binfo->vram, binfo->scrnx, 8, 8, COL8_FFFFFF, "ABC 123");
+  putfonts8_asc(binfo->vram, binfo->scrnx, 31, 31, COL8_000000, "Haribote OS.");
+  putfonts8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "Haribote OS.");
 
   for (;;) {
     io_hlt();
@@ -131,6 +129,16 @@ void init_screen(char *vram, int xsize, int ysize) {
            ysize - 3);
   boxfill8(vram, xsize, COL8_FFFFFF, xsize - 3, ysize - 24, xsize - 3,
            ysize - 3);
+}
+
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c,
+                   unsigned char *s) {
+  extern char hankaku[4096];
+
+  for (; *s != 0x00; s++) {
+    putfont8(vram, xsize, x, y, c, hankaku + *s * 16);
+    x = x + 8;
+  }
 }
 
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font) {
