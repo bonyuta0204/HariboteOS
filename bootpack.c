624401331db7cc8115c31eea15cff7eb3b47075e
Yuta Nakamura <nakamurayuta0204@gmail.com>
2024-02-18 01:01:42 +0900

    fetch mouse data

--- bootpack.c
+++ bootpack.c
@@ -12,6 +12,7 @@ void HariMain(void) {
   struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
   char s[40], mcursor[256], keybuf[32], mousebuf[128];
   int mx, my, i;
+  unsigned char mouse_dbuf[3], mouse_phase;
 
   init_gdtidt();
   init_pic();
@@ -35,6 +36,7 @@ void HariMain(void) {
   putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
   enable_mouse();
+  mouse_phase = 0; /* マウスの0xfaを待っている段階へ */
 
   for (;;) {
     io_cli();
@@ -52,9 +54,30 @@ void HariMain(void) {
         /** マウス入力が存在 */
         i = fifo8_get(&mousefifo);
         io_sti();
-        sprintf(s, "%d", i);
-        boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 60, 31);
-        putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+
+        switch (mouse_phase) {
+        case 0:
+          if (i == 0xfa) {
+            mouse_phase = 1;
+          };
+          break;
+        case 1:
+          mouse_dbuf[0] = i;
+          mouse_phase = 2;
+          break;
+        case 2:
+          mouse_dbuf[1] = i;
+          mouse_phase = 3;
+          break;
+        case 3:
+          mouse_dbuf[2] = i;
+          mouse_phase = 1;
+          sprintf(s, "%d %d %d", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
+          boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16,
+                   32 + 8 * 8 - 1, 31);
+          putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+          break;
+        }
       }
     }
   }
